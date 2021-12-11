cde3e4e2b8eb721da351f0b72dcc0c39592be003
omerserdarkose <omerserdarkose@gmail.com>
2021-12-11 19:32:48 +0300

    Define EfEntityRepositoryBase class

--- HelenSposa.Core/DataAccess/EntityFramework/EfEntityRepositoyBase.cs
+++ HelenSposa.Core/DataAccess/EntityFramework/EfEntityRepositoyBase.cs
@@ -9,34 +9,55 @@ using System.Threading.Tasks;
 
 namespace HelenSposa.Core.DataAccess.EntityFramework
 {
-    //tum entity lerde yapilan islemler ayni oldugu icin temel sinifta toplayip generic calisabilmek icin bu sinif olusturluyor
+    //tum entity lerde yapilan sabit islemler oldugu icin temel sinifta toplayip generic calisabilmek amaciyla bu sinif olusturluyor
     class EfEntityRepositoyBase<TEntity, TContext> : IEntityRepository<TEntity>
         where TEntity : class, IEntity, new()
         where TContext : DbContext, new()
     {
         public void Add(TEntity entity)
         {
-            throw new NotImplementedException();
+            using (var context=new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
         }
 
         public void Delete(TEntity entity)
         {
-            throw new NotImplementedException();
+            using (var context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
         }
 
         public TEntity Get(Expression<Func<TEntity, bool>> filter)
         {
-            throw new NotImplementedException();
+            using (var context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
         }
 
         public List<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null)
         {
-            throw new NotImplementedException();
+            using (var context=new TContext())
+            {
+                return filter == null ? context.Set<TEntity>().ToList() : context.Set<TEntity>().Where(filter).ToList();
+            }
         }
 
         public void Update(TEntity entity)
         {
-            throw new NotImplementedException();
+            using (var context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
         }
     }
 }
--- HelenSposa.Core/DataAccess/IEntityRepository.cs
+++ HelenSposa.Core/DataAccess/IEntityRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HelenSposa.Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
