9ff4b5d4a103be24d78291a0d2fcb0aef9f93d2a
omerserdarkose <omerserdarkose@gmail.com>
2021-12-11 18:46:36 +0300

    Add EfEntityRepositoryBase
    EntityRepositoryBase file created for Entity Framework

---
+++ HelenSposa.Core/DataAccess/EntityFramework/EfEntityRepositoyBase.cs
@@ -0,0 +1,42 @@
+﻿using HelenSposa.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelenSposa.Core.DataAccess.EntityFramework
+{
+    //tum entity lerde yapilan islemler ayni oldugu icin temel sinifta toplayip generic calisabilmek icin bu sinif olusturluyor
+    class EfEntityRepositoyBase<TEntity, TContext> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+        public void Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
